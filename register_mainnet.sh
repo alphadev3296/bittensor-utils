c75638a6d1f0d3d2a8b47da850733077e1fec865
alpha <alpha5611331@gmail.com>
2025-07-13 10:15:46 -0400

    initial commit

---
+++ register_mainnet.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# DESCRPTION: Register miner on mainNet
+
+# load env
+set -a
+source .env
+set +a
+
+# print env
+echo "Wallet: ${WALLET}"
+echo "Hotkey: ${HOTKEY}"
+
+# activate python venv
+source .venv/bin/activate
+
+# register miner
+btcli subnet register --wallet.name ${WALLET} --wallet.hotkey ${HOTKEY} --subtensor.network test --netuid 116
---
+++ register_testnet.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# DESCRPTION: Register miner on testNet
+
+# load env
+set -a
+source .env
+set +a
+
+# print env
+echo "Wallet: ${WALLET_TESTNET}"
+echo "Hotkey: ${HOTKEY_TESTNET}"
+
+# activate python venv
+source .venv/bin/activate
+
+# register miner
+btcli subnet register --wallet.name ${WALLET_TESTNET} --wallet.hotkey ${HOTKEY_TESTNET} --subtensor.network test --netuid 116
